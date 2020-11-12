3d611e258bb4e9f7161f6e24c2e9a53627d643fe
Natsumi Matsuzawa <matsuzawa-nm@bhopari.onmicrosoft.com>
2020-11-12 18:38:05 +0900

    forの波線は解決。 $i問題が発生。

--- シェル問題/shell/04_count-line-numbers/test.sh
+++ シェル問題/shell/04_count-line-numbers/test.sh
@@ -20,4 +20,18 @@
 #     # read -r < test.txt | wc -l
 # done
 
-< test.txt wc -l
+# < test.txt wc -l
+
+declare -ri zero=1
+# 
+# パラメータが入力されているかを判定する
+function enter_parameter_or_not() {
+    declare -ri total=$1
+
+    if [[ total -lt ${zero} ]]; then
+        echo "$#"
+    fi
+}
+
+enter_parameter_or_not
+echo "OK"
